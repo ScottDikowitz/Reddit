b8677b5d76d0b6c7b41962ebc7fc27dc6879ea94
App Academy Student <appacademy@Apps-Mac-mini-9.local>
2015-10-23 11:28:54 -0400

    Add auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user =  User.find_by_credentials(params[user][username], params[user][password])
+    if @user
+      log_in!(@user)
+      redirect_to user_url(@user.id)
+    else
+      flash[:error] = ["Invalid username or password"]
+      render new
+    end
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Reddit::Application.routes.draw do
   resources :users, only: [:create, :new, :show]
+
+  resource :session, only: [:create,:new, :destroy]
 end
